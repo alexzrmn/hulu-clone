69fc71e0054ead1a49bde0f3bf26e236b3827573
Alexis Roman <alexisrmnpro@gmail.com>
2021-11-04 22:48:52 +0100

    hulu-clone

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import HeaderItem from "./HeaderItem";
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon, 
+    LightningBoltIcon, 
+    SearchIcon, 
+    UserIcon
+} from "@heroicons/react/outline";
+
+function Header() {
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title='HOME' Icon={HomeIcon}/>
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
+                <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+                <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
